ee43a45695ffac2c81f02fcbfd74231bb2413cb3
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2022-11-03 23:21:01 +0000

    update input validation function header and its use in main accordingly
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- helpers.py
+++ helpers.py
@@ -5,17 +5,21 @@ This file contains helper functions that will aid the running program
 
 import re
 
-'''
-This function takes the form inputs and validates it by searching for the specified regex pattern
-and ensuring that the length of the input is either 9 or 11.
 
-:param digital_address: the source or destination user address the user wants to search for
-:return: bool
-'''
+def is_valid_input(source_address: str, dest_address: str) -> bool:
+    """
+    This function takes the form inputs and validates it by searching for the specified regex pattern
+    and ensuring that the length of the input is either 9 or 11.
 
+    :param source_address: the source address the user wants to search from
+    :param dest_address: the destination address the user will search for
+    :return: bool
+    """
+    source_match = re.search("^[A-Z]{2}-[0-9]{3}-[0-9]{4}$||^[A-Z]{2}[0-9]{7}$", source_address)
+    dest_match = re.search("^[A-Z]{2}-[0-9]{3}-[0-9]{4}$||^[A-Z]{2}[0-9]{7}$", dest_address)
 
-def is_valid_input(digital_address: str) -> bool:
-    match = re.search("^[A-Z]{2}-[0-9]{3}-[0-9]{4}$||^[A-Z]{2}[0-9]{7}$", digital_address)
-    if match and (len(digital_address) == 9 or len(digital_address) == 11):
+    if (source_match and dest_match) and (len(source_address) == 9 or len(source_address) == 11 and len(dest_address) ==
+                                          9 or len(dest_address) == 11):
         return True
+
     return False
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, escape, request
+import requests
+from helpers import is_valid_input
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def landing_page():
     return render_template('index.html')
+
+
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+    error: str = None
+    if request.method == 'POST':
+        if is_valid_input(escape(request.form['source-address']), escape(request.form['destination-address'])):
+            source_address: str = escape(request.form['source-address'])
+            dest_address: str = escape(request.form['destination-address'])
+            return '<h1> Source: {} Destination: {} </h1>'.format(source_address, dest_address)
+        else:
+            error = 'Invalid search input(s)'
+    elif request.method == 'GET':
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
